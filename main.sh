aaa8e70b45de9004df1e5e1164413cd7a855ca2c
YuigaWada <yuigawada@gmail.com>
2022-11-21 21:40:00 +0900

    [update] format

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ MATTERPORT_DATA_DIR=$1
 
 for scanId in `ls $MATTERPORT_DATA_DIR`
 do
+    # prepare
     path="$MATTERPORT_DATA_DIR/$scanId/matterport_skybox_images"
     viewIds=""
     for file in `ls $path`:
@@ -12,12 +13,16 @@ do
         arr=`echo $file | tr "_" "\n"`
         for x in $arr; do viewIds="$viewIds\n$x"; break; done
     done
+
+    # create image
     for viewId in `echo $viewIds | uniq`
-        do
-            echo "> " $scanId $viewId
-            python3 create_image.py $scanId $viewId data/panorama
-            echo "... done"
+    do
+        echo "> " $scanId $viewId
+        python3 create_image.py $scanId $viewId data/panorama
+        echo "... done"
     done
+    
+    # update ids.json
     echo "update ids.json"
     echo $viewIds | uniq | tr "\n" " " > .tmp
     python3 create_json.py ids.json $scanId
