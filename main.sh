95bfb125359b86e0cdacf4906ef3b91e21f4c0a3
YuigaWada <yuigawada@gmail.com>
2022-11-21 19:30:40 +0900

    [fix] make sure to create correct images

--- main.sh
+++ main.sh
@@ -6,16 +6,16 @@ MATTERPORT_DATA_DIR=$1
 for scanId in `ls $MATTERPORT_DATA_DIR`
 do
     path="$MATTERPORT_DATA_DIR/$scanId/matterport_skybox_images"
+    viewIds=""
     for file in `ls $path`:
     do
         arr=`echo $file | tr "_" "\n"`
-        viewIds=""
-        for x in $arr; do viewIds="$viewIds $x"; break; done
-        for viewId in `echo $viewIds | uniq`
+        for x in $arr; do viewIds="$viewIds\n$x"; break; done
+    done
+    for viewId in `echo $viewIds | uniq`
         do
             echo "> " $scanId $viewId
             python3 create_image.py $scanId $viewId data/panorama
             echo "... done"
-        done
     done
 done
